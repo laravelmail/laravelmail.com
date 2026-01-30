f564df32c8ea0b404bf6f21250f1ad768f8c7353
Bogdanel Stefan <stefan@ourrainwater.com>
2026-01-30 08:05:14 +0000

    Content improvements

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -68,6 +68,15 @@ export default {
         },
       },
     },
+    screens: {
+      'xs': '320px',
+      'ph': '480px',
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
